d43b9bbd4a1096dab562e18ad77be83de2aa5b6b
chechurov <dima-0794@mail.ru>
2019-03-08 17:32:52 +0300

    1.3. JavaDoc. Code convention[#119121]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate.
+* @author Dmitry Chechurov (dima-0794@mail.ru)
+*/
 public class Calculate{
+	/**
+	* Main. Вывод строки в консоль.
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
