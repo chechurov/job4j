ee2b58f64ceba600065f0f2100d8321a6b2320dc
chechurov <dima-0794@mail.ru>
2019-03-08 19:57:11 +0300

    1.5. Checkstyle[#119123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,12 +4,12 @@ package ru.job4j.calculate;
 * Calculate.
 * @author Dmitry Chechurov (dima-0794@mail.ru)
 */
-public class Calculate{
+public class Calculate {
 	/**
 	* Main. Вывод строки в консоль.
 	* @param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
