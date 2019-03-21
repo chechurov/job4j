c3964f9377b7801706e5a72e660ec6ee62d51091
chechurov <dima-0794@mail.ru>
2019-03-21 12:36:41 +0300

    Пазлы. Тестовое задание.[#119128] (Правка метода isWin)

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -41,8 +41,8 @@ public class Logic {
         boolean result = cells.length > 0;
         for (Cell cell : cells) {
             if (this.findBy(cell) != -1) {
-               result = false;
-               break;
+                result = false;
+                break;
             }
         }
         return result;
@@ -66,63 +66,18 @@ public class Logic {
         return rst;
     }
 
-    //Первый вариант isWin().
-    /*public boolean isWin() {
-        int[][] table = this.convert();
-        boolean result = false;
-        int[] count = new int[table.length * 2];
-        //Проходим весь массив и записываем сколько найдено единиц в каждой строке и столбце.
-        for (int i = 0; i != table.length; i++) {
-            for (int j = 0; j != table.length; j++) {
-                if (table[i][j] == 1) {
-                    count[i] += 1; //Добавляем, что нашли единицу в i-ой строке.
-                    count[table.length + j] += 1; //Добавляем, что нашли единицу в j-ой столбце.
-                }
-            }
-        }
-        //Сверяем количество единиц в столбцах и строках с нужным количеством.
-        for (int i = 0; i != count.length; i++){
-            if (count[i] == table.length) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }*/
-
-    //Второй вариант isWin().
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
-        //Подсчитываем количество единиц в i-ой строке.
-        //В lastColumn хранится индекс столбца, в котором была найдена последняя единица.
-        for (int i = 0, countInLine = 0, lastColumn = 0; i != table.length; i++, countInLine=0) {
+        for (int i = 0; i != table.length; i++) {
+            int checkRow = 1;
+            int checkColumn = 1;
             for (int j = 0; j != table.length; j++) {
-                if (table[i][j] == 1) {
-                    countInLine += 1;
-                    lastColumn = j;
-                }
+                checkRow *= table[i][j];
+                checkColumn *= table[j][i];
             }
-            //Если в i-ой строке все единицы, то выводим true.
-            if (countInLine == table.length) {
+            if (checkRow == 1 || checkColumn == 1) {
                 result = true;
-                break;
-            }
-            //Если в i-ой строке больше одной единицы, то выводим false(дальше нет смысла проверять).
-            else if (countInLine > 1) {
-                result = false;
-                break;
-            }
-            //Если в i-ой строке одна единица, то проверяем только этот столбец где найдена эта единица.
-            else if (countInLine == 1) {
-                result = true;
-                //Если в столбце не все единицы, то выводим false.
-                for (int k = 0; k != table.length; k++) {
-                    if (table[k][lastColumn] != 1) {
-                        result = false;
-                        break;
-                    }
-                }
                 break;
             }
         }
